77b081dc94508bf8e16913daa6b60c80545c6abf
vsemenov <Vyacheslav.Semenov@slsoft.ru>
2025-02-20 16:33:15 +0500

    make reg page

--- src/main/java/ru/germes/plus/site/controller/Main.java
+++ src/main/java/ru/germes/plus/site/controller/Main.java
@@ -1,13 +1,14 @@
 package ru.germes.plus.site.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class Main {
 
     @GetMapping
     public String hello() {
-        return "Привет";
+        return "index";
     }
 }
