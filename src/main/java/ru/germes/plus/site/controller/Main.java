329b7bf0396e9ed937fb473653a63cf8e4db7450
TeflonDervish <slava.semenov03@mail.ru>
2025-02-23 12:29:28 +0500

    add templates

--- src/main/java/ru/germes/plus/site/controller/Main.java
+++ src/main/java/ru/germes/plus/site/controller/Main.java
@@ -1,14 +1,20 @@
 package ru.germes.plus.site.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class Main {
 
-    @GetMapping
-    public String hello() {
-        return "index";
+    @GetMapping("/login")
+    public String getLogin(Model model) {
+        return "login.html";
+    }
+
+    @GetMapping("/registration")
+    public String getRegistration(Model model) {
+        return "registration.html";
     }
 }
