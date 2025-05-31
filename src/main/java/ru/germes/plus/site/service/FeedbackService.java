b482d3e99f0be893ada643ebaeaca73ab2ccdecf
teflondervish <slava.semenov03@mail.ru>
2025-05-31 14:40:17 +0500

    Подправил ошибки

--- src/main/java/ru/germes/plus/site/service/FeedbackService.java
+++ src/main/java/ru/germes/plus/site/service/FeedbackService.java
@@ -3,7 +3,6 @@ package ru.germes.plus.site.service;
 import lombok.AllArgsConstructor;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.germes.plus.site.model.feedbacks.FeedbackOnProductForIndividual;
 import ru.germes.plus.site.model.persons.IndividualPerson;
@@ -30,7 +29,7 @@ public class FeedbackService {
         feedbackOnProductForIndividual.setIndividualPerson(individualPerson);
         feedbackOnProductForIndividual.setProductForIndividual(productForIndividual);
         feedbackOnProductForIndividual.setText(text);
-        feedbackOnProductForIndividual.setGrade(5);
+        feedbackOnProductForIndividual.setGrade(5.0);
         feedbackOnProductForIndividual.setDate(LocalDate.now());
 
         return feedbackRepository.save(feedbackOnProductForIndividual);
